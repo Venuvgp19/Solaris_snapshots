c83e45b7ad12b432507606585fd1063c6f4ba309
Venugopal <venugopal.p@unfi.com>
2021-08-04 05:50:23 -0500

    adding project files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Snapshots - Solaris Servers - Pre-requisites
+1) /DR_NAS should be mounted on the server where you run this script.
+2) This script should be run on Global servers since we take snapshots from global server.
+3) The local zones for which the snapshot should be taken is hardcoded/
+
---
+++ snapshost.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+
+os.system('cat /dev/null > /tmp/poolist.txt')
+Server_list = ['wpsun464']
+pool_list = []
+for i in Server_list:
+    os.system('zpool list | grep -i %s'%(i) + ' | awk "{print \$1}" >> /tmp/poolist.txt')
+
+def create_dir_in_DRNAS(Server_list):
+    for i in Server_list:
+            os.system('mkdir -p /DR_NAS/Solaris/Snapshots/%s'%(i))
+
+def copy_snapshot(pool):
+    size = len(pool)
+# Slice string to remove last 2 characters from string
+    server = pool[:size - 2]
+    os.system('zfs send -Rv %s'%(pool) + '@golden > /DR_NAS/Solaris/Snapshots/%s'%(server) + '/%s'%(pool) + '.snapshot')
+
+d_file = open("/tmp/poolist.txt", "r")
+for line in d_file:
+    stripped_line = line.strip('\n')
+    pool_list.append(stripped_line)
+d_file.close()
+print(pool_list)
+
+create_dir_in_DRNAS(Server_list)
+
+for pools in pool_list:
+    os.system('zfs snapshot -r %s'%(pools) + '@golden')
+    copy_snapshot('%s'%(pools))
+
+##copy snapshot
+
+for pools in pool_list:
+    os.system('zfs destroy -r %s'%(pools) + '@golden')
+
